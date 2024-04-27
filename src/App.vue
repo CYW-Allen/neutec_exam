e830a7c1320911f55293b5e8930c1bb415c30461
CYW-Allen <wen900445@gmail.com>
2024-04-27 17:52:09 +0800

    init proj

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="appHeader">
+    <button type="button" class="btn">
+      <i class="fa-solid fa-bars"></i>
+    </button>
+  </div>
+  <div class="appBody">
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.appHeader {
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: center;
+  padding: 5px;
+}
+
+.appBody {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgray;
+  padding: 5px;
+}
+</style>
