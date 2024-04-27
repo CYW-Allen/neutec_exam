a7d1a63bbe747bfb022f049dc9798621c72e22a0
CYW-Allen <wen900445@gmail.com>
2024-04-27 20:00:52 +0800

    feat: implement dropdown list

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container" id="sideMenuContainer" :style="`left: ${showSideMenu ? '50%' : '100%'}`">
+    <div class="dropDown">
+      <select name="dataList" v-model="selected">
+        <option value="">Choose an option</option>
+        <option v-for="opt, i in dropDownList" :key="`opt${i}`" :value="opt.dir">{{ opt.text }}</option>
+      </select>
+    </div>
+    
     <DataItem :data-list="dataList" :level-num="0" />
   </div>
 </template>
 
 <script setup>
-import { inject, watch, ref, provide } from 'vue';
+import { inject, watch, ref, provide, computed } from 'vue';
 import { animateEle } from '../utils/animateHandler';
 import DataItem from './DataItem.vue';
 
@@ -14,6 +21,29 @@ const dataList = inject('dataList');
 
 const clientRecord = window.localStorage.getItem('expendList') || '';
 const expanded = ref(clientRecord.split(','));
+const dropDownList = computed(() => {
+  const list = [];
+  const nextVisit = [{ dir: '', items: dataList.value.slice() }];
+
+  while (nextVisit.length) {
+    const { dir, items } = nextVisit.pop();
+
+    items.forEach((item) => {
+      const curDir = `${dir !== '' ? `${dir},` : ''}${item.key}`
+
+      list.push({ dir: curDir, text: item.text });
+      if (item.children instanceof Array) {
+        nextVisit.push({
+          dir: curDir,
+          items: item.children.slice(),
+        });
+      }
+    });
+  }
+
+  return list;
+});
+const selected = ref(clientRecord);
 
 function expandList(data, level) {
   expanded.value[level] = data.key;
@@ -28,9 +58,15 @@ watch(showSideMenu, (v) => {
 });
 
 watch(expanded, (v) => {
-  window.localStorage.setItem('expendList', v.join(','));
+  const listStr = v.join(',');
+
+  if (listStr !== selected.value) selected.value = listStr;
+  window.localStorage.setItem('expendList', listStr);
 }, { deep: true });
 
+watch(selected, (v) => {
+  expanded.value = v.split(',');
+});
 </script>
 
 <style scoped>
